d4f73d1630f301f4ebf4f723c0ad2be13825d7df
Josh.R <Josh@Joshs-Macbook-Pro-2.local>
2017-01-16 21:16:42 -0500

    Battle Scene
    -Added and created a battle scene
    -Added buttons
    -Moved camera down

---
+++ GameStudio_2/Assets/Scripts/Character/warriorClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warriorClass : MonoBehaviour {
+
+
+	public static int healthPoints = 100;
+	public static int maxHealthPoints = 100;
+
+
+
+
+
+
+}
