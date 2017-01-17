1b2f1a88505bd51e8b5069d4c902604d40addc7b
josh16 <josh.randall16@gmail.com>
2017-01-17 00:01:31 -0500

    Character Class Scripts
    -Created a base class Script
    -Created a Warrior class Script
    -Created a Goblin class script
    -Created a GUI script to test some of the UI

---
+++ GameStudio_2/Assets/Scripts/Character/BaseWarriorClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Inheriting from the BaseClass script, not monobehaviour
+public class BaseWarriorClass : BaseClass {
+
+	
+    public BaseWarriorClass()
+    {
+        CharacterClassName = "Warrior";
+        CharacterClassDescription = "A Strong powerful Warrior!!";
+
+        Stamina = 10;
+        Hp = 80;
+        Strength = 15;
+
+    }
+
+
+}
