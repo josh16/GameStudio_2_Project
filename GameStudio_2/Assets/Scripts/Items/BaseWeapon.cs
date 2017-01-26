899f8aabe888c46ee48b82214caae28b458c9ff9
Josh.R <Josh@Joshs-Macbook-Pro-2.local>
2017-01-26 14:52:08 -0500

    New Scripts Added! and Create a Battle Scene!!!
    -potion Script and also create potion script
    -Fixed a few of the old scripts for Equipment, CreateNewEquipment and
    so on
    -Repositioned Player hero and moved player hero inside Forest
    -Added some Assets to the battle scene

---
+++ GameStudio_2/Assets/Scripts/Items/BaseWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWeapon : BaseStatItem { // This is inheriting from BaseStatItem <- BaseItem 
+
+
+	//Enumerator for different weapon types
+	public enum WeaponTypes
+	{
+		SWORD,
+		HAMMER,
+		STAFF,
+		BOW,
+		AXE
+		//Leave extra room here if you want to add extra weapons for the future
+
+	}
+
+	private WeaponTypes weaponType;
+
+
+	public WeaponTypes WeaponType
+	{
+		get { return weaponType ; }
+		set { weaponType = value; }
+	}
+
+
+}
---
+++ GameStudio_2/Assets/Scripts/Items/CreateNewWeapon.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNewWeapon : BaseWeapon {
+
+	private BaseWeapon newWeapon;
+
+	public void Start()
+	{
+		CreateWeapon();
+
+		Debug.Log(newWeapon.ItemName);
+		Debug.Log(newWeapon.ItemDescription);
+		Debug.Log(newWeapon.ItemID.ToString());
+		Debug.Log(newWeapon.WeaponType.ToString());
+		Debug.Log(newWeapon.Stamina.ToString());
+	}
+
+	//Create a WeaponTypes function
+	public void CreateWeapon()
+	{
+		//Variables for ID, description,name, and range rumber 
+		newWeapon = new BaseWeapon();
+		newWeapon.ItemName = "W" + Random.Range (1, 100);
+		newWeapon.ItemDescription = "A new Weapon!";
+		newWeapon.ItemID = Random.Range (1, 100);
+
+		//Accessing the different types of stats
+		newWeapon.Strength = Random.Range (1,11);
+		newWeapon.Stamina = Random.Range (1, 11);
+
+		//Calling the choose Weapon function here
+		ChooseWeapon ();
+	}
+		
+
+//Function to choose Weapon
+	public void ChooseWeapon()
+	{
+		//Temp value
+		int randomTemp = Random.Range (1, 6);
+
+
+		//Weapon numbers 
+		if (randomTemp == 1) {
+
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.SWORD;
+		} 
+		else if (randomTemp == 2) {
+
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.HAMMER;
+		}
+
+		else if (randomTemp == 3) {
+
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.STAFF;
+		}
+
+		else if (randomTemp == 4) {
+
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.BOW;
+		}
+
+		else if (randomTemp == 5) {
+
+			newWeapon.WeaponType = BaseWeapon.WeaponTypes.AXE;
+		}
+
+
+
+	}
+
+
+}
